89c0b61442a5b3688195e03ea991c5297fa168ba
Akash Agarwal <akash.software.1982@gmail.com>
2017-03-02 00:59:19 -0800

    checked-in service filder which needs to be deleted

--- app/app.component123.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core'
-@Component({
-    selector : 'akash',
-    template:`<h1>{{name}}</h1>`
-})
-export class appcomponent123{
-
-    name:string = "This is akash component - horrey";
-}
